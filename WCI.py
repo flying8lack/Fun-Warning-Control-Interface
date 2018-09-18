fb798b05ad17bb79cdc29f5f69c7722b51e3ede0
flying8lack <19859499+flying8lack@users.noreply.github.com>
2018-09-18 12:54:19 +0100

    Update WCI.py

--- WCI.py
+++ WCI.py
@@ -1,10 +1,25 @@
-from tkinter import Tk, Button, Entry, messagebox, Label
+from tkinter import Tk, Button, Entry, messagebox, Label, Menu, NORMAL
+from tkinter.ttk import Progressbar
 
 class App:
     def __init__(self, master):
         self.master = master
         master.title("Warning Control Interface")
-        master.geometry("300x110")
+        master.geometry("300x170")
+
+        self.count_bar = Progressbar(master, mode='determinate', maximum=10)
+        self.count_bar.grid(row=8, column=50, pady=10)
+
+        self.ex_btn = Button(master, text="Fire!", command=self.warn, fg="red")
+
+        #menu setup
+        self.menu_obj = Menu(master)
+        self.menu_obj.add_command(label="Hello", command=self.hello)
+        self.menu_obj.add_command(label="Reset", command=self.reset)
+        self.menu_obj.add_command(label="Fill", command=self.fill)
+        #master config
+        master.config(menu=self.menu_obj)
+
 
         self.message_title = Entry(master)
         self.message_title.grid(row=1, column=50)
@@ -18,13 +33,27 @@ class App:
         self.message_content_text = Label(master, text="Message content: ")
         self.message_content_text.grid(row=3, column=40)
 
-        self.wrn_btn = Button(master, text="Generate the warning!", command=self.warn, fg="red")
-        self.wrn_btn.grid(row=7, column=50)
+        self.wrn_btn = Button(master, text="Generate the warning!", command=self.warn)
+        self.wrn_btn.grid(row=7, column=50, pady=10)
 
     def warn(self):
-        self.count += 1
+        self.count_bar['value'] += 1
         messagebox.showwarning(self.message_title.get(), self.message_content.get())
-        self.wrn_btn.configure(text="Count: "+str(self.count))
+
+        if self.count_bar['value'] > 8:
+            self.ex_btn.config(state=NORMAL)
+            self.ex_btn.grid(row=10, column=50)
+
+    def hello(self):
+        messagebox.showwarning("!", "\"Hello there\" - codsworth")
+
+    def reset(self):
+        self.count_bar['value'] = 0
+        self.ex_btn.grid_forget()
+
+    def fill(self):
+        self.count_bar['value'] = 10
+        self.warn()
 
 root = Tk()
 page = App(root)
